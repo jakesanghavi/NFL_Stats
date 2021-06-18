9c85c438f931e85bf3b2771131224ee9337d5c63
Jake Sanghavi <57878447+jakesanghavi@users.noreply.github.com>
2021-06-18 16:56:09 -0400

    Update and rename ScrapeJSONS.py to scrape_SportRadar_games.py

--- scrape_SportRadar_games.py
+++ scrape_SportRadar_games.py
@@ -18,8 +18,8 @@ header_data  = {
    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
 }
 
-
-key = "gjyp4a3tjx4x97ytwzq754ex"
+# Put your API key here
+key = ""
 
 ids = pd.read_csv('GAME_API_IDS')
 ids = pd.DataFrame(ids.index.values)
