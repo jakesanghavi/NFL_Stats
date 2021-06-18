8e9d64e41be6c3ac001fc9d9a255335db809d6f7
Jake Sanghavi <57878447+jakesanghavi@users.noreply.github.com>
2021-06-18 16:13:41 -0400

    Update and rename SportRadar Updater.py to SportRadar_updater.py

--- SportRadar_updater.py
+++ SportRadar_updater.py
@@ -16,21 +16,3 @@ pd.set_option('display.max_columns', 300)
 data = data.loc[(data.play_type.isin(['penalty','pass','rush', 'conversion']))]
 
 data.to_csv('SportRadar_' + year + '.csv', index=False)
-
-# data.drop(['play_id', 'play_type'],
-#           axis=1, inplace=True)
-
-# data.rename(columns={'play_ref':'play_id'}, inplace=True)
-
-# r_data = pd.read_csv('reg_season_play_by_play_2019.csv', low_memory=False)
-#
-# r_data = r_data.loc[r_data.play_type_nfl != 'TIMEOUT']
-#
-# r_data.to_csv('regular_season_play_by_play_2019.csv')
-#
-# print(len(r_data))
-# print(len(data))
-
-# full_reg_pbp_2019 = pd.merge(data, r_data, on='play_id', how='left')
-#
-# print(full_reg_pbp_2019)
